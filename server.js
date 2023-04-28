9d8581776a9caecad275465854e814f19dbbc55a
indyya <indyya@ad.unc.edu>
2023-04-27 21:02:52 -0400

    fixed hopefully pt4

--- server.js
+++ server.js
@@ -17,14 +17,11 @@ const port = args.port || 3000;
 //
 // // If there is an error, put it on the console error and return. 
 // // Do not be nice about exiting.
-d = fs.readFile('./public/index.html','utf8', (err, data) => {
-	if(err) {
-		console.error(err);
-		return;
-	}
-	console.log(d);
-});
-	
+fs.readFile('./public/index.html', (err, data) => {
+if(err) {
+   console.error(err);
+   return;
+}
 
 // // Define a const `server` as an arrow function using http.createServer. 
 // // Use the documentation for the node.js http module. 
@@ -36,7 +33,7 @@ d = fs.readFile('./public/index.html','utf8', (err, data) => {
 	 res.statusCode = 200;
 	 res.setHeader('Content-Type', 'text/html');
 	 res.end(data);
- });
+ })
 // // Start the `server` const listening on the port defined by argument in your `port` const. 
 // // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port);
