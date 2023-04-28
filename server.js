57947e248622be6fa7640e7f6af5995cc3a4801d
indyya <indyya@ad.unc.edu>
2023-04-27 20:32:12 -0400

    fixed hopefully

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ fs.readFile("./public/index.html",'utf8', (err, data) => {
 		 console.error(err);
 		 return;
 	 }
-
+});
 
 // // Define a const `server` as an arrow function using http.createServer. 
 // // Use the documentation for the node.js http module. 
@@ -30,15 +30,13 @@ fs.readFile("./public/index.html",'utf8', (err, data) => {
 // // 1. status code 200, 
 // // 2. set a header with content type `text/html`, and 
 // // 3. end with the data that you are reading in from ./public/index.html.
- const server = http.createServer((_, res) => {
+ const server = http.createServer((req, res) => {
 	 res.statusCode = 200;
 	 res.setHeader("Content-Type", "text/html");
 	 res.end(data);
- })
+ });
 // // Start the `server` const listening on the port defined by argument in your `port` const. 
 // // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
- });
+server.listen(port);
+console.log('Server listening on port' +port);
 // // That's it! You're all done!
-});
