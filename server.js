41cb9c2bd30d6c9f06eaf7e0baac9e0e4f625067
indyya <indyya@ad.unc.edu>
2023-05-01 18:59:57 -0400

    Last try before going to OH

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const http = require("http");
 const fs = require("fs");
 const minimist = require("minimist");
+
 const args = minimist(process.argv.slice(2));
-const port = args.port || 300;
+const port = args.port || 3000;
 
 fs.readFile('./public/index.html','utf8', (err, data) =>{ 
       if (err) {
 	 console.error(err);
-	 return;
+	 process.exit(1);
       }
       console.log(data);
 
       const server = http.createServer((req, res) => {
-	//res.statusCode = 200;
-	//res.setHeader('Content-Type', 'text/html');
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	readIndex((data)=> {
-		res.end(data);
-     })
- })
-server.listen(port,() => {
-   console.log('Server listening on port ${port}');
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(data);
+      });
+      server.listen(port, () => {
+	      console.log('Server listening on port ${port}');
+      });
 });
 
